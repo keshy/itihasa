af89502719d9a18a1e47411fe6f9361d9a649a2f
KNarayan <schoolhouse@Krishnans-Mac-mini.local>
2025-04-06 17:49:54 -0700

    Fixing image overlay

--- src/publisher/youtube.py
+++ src/publisher/youtube.py
@@ -4,37 +4,40 @@ from moviepy.editor import AudioFileClip, CompositeAudioClip
 import moviepy.video.fx.all as vfx
 import moviepy.audio.fx.all as afx
 # merge 2 audio files into one but with one of them on lower volume
+from moviepy.video.VideoClip import ImageClip
 from moviepy.video.compositing.concatenate import concatenate
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
 
 
 def merge_audio_files(file1, file2, volume=0.5):
-    audio1 = AudioFileClip(file1)
-    audio2 = AudioFileClip(file2)
-    audio2 = audio2.volumex(volume)
-    audio2 = afx.audio_loop(audio2, duration=audio1.duration)
-    final_audio = CompositeAudioClip([audio1, audio2])
-    final_audio = final_audio.set_duration(audio1.duration)
-    final_audio.write_audiofile('1_merged.wav', fps=44100)
-    audio1.close()
-    audio2.close()
-    final_audio.close()
-    # create video with the merged audio and a bunch of images slowly fading in and rotating
-    # write code
+    # audio1 = AudioFileClip(file1)
+    # audio2 = AudioFileClip(file2)
+    # audio2 = audio2.volumex(volume)
+    # audio2 = afx.audio_loop(audio2, duration=audio1.duration)
+    # final_audio = CompositeAudioClip([audio1, audio2])
+    # final_audio = final_audio.set_duration(audio1.duration)
+    # final_audio.write_audiofile('1_merged.wav', fps=44100)
+    # audio1.close()
+    # audio2.close()
+    # final_audio.close()
+    # # create video with the merged audio and a bunch of images slowly fading in and rotating
+    # # write code
     image_folder = './image'
     image_files = sorted([os.path.join(image_folder, f)
                           for f in os.listdir(image_folder)
                           if f.endswith(('.jpg', '.jpeg', '.png', '.gif'))])
 
+    # load audio file from disk
+
+    ad = AudioFileClip('./1_merged.wav')
     if not image_files:
         print(f"No valid image files found in: {image_folder}")
         return
-    fps = 44100
-    clip = ImageSequenceClip(image_files, fps=fps)
+    clip = ImageClip(img=image_files[0], duration=ad.duration)
     # Set the audio of the video clip to the loaded audio clip
-    clip = clip.set_audio(final_audio)
-    clip.write_videofile("youtube_ready_video", codec='mpeg4')
-    final_audio.close()
+    clip = clip.set_audio(ad)
+    clip.write_videofile("youtube_ready_video.mp4", codec='mpeg4', fps=1, audio_codec='aac', threads=4)
+    ad.close()
     clip.close()
 
     return "youtube_ready_video"
